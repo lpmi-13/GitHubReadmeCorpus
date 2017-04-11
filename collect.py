de587a10b66aa0019117c760b34571b1498a8b63
lpmi-13 <leskis@gmail.com>
2017-04-11 21:56:15 +0100

    add requirements.txt

--- collect.py
+++ collect.py
@@ -4,9 +4,15 @@ import requests, base64, re, yaml
 with open('settings.yaml', 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
 
-g = GitHub(cfg['user'], cfg['password'], timeout=200, per_page=30)
+g = Github(cfg['user'], cfg['password'], timeout=200, per_page=30)
 
 results = g.get_repos()
 
 for repo in results:
     print repo.html_url
+
+    readme = repo.get_readme()
+    data = base64.b64decode(readme.content)
+
+    print '###################'
+    print 
