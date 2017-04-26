cd215490a5d6389f9a8bf306fb5172e389b1e7b3
lpmi-13 <leskis@gmail.com>
2017-04-26 22:59:47 +0100

    update with check on repo id

--- collect.py
+++ collect.py
@@ -1,5 +1,6 @@
 from github import Github
 import requests, base64, yaml
+from pymongo import MongoClient
 
 with open('settings.yaml', 'r') as ymlfile:
     cfg = yaml.load(ymlfile)
@@ -13,6 +14,8 @@ publicly available repository on GitHub
 '''
 results = g.get_repos()
 
+db = MongoClient().github
+
 write_text = open('readmeText.txt', 'w')
 write_text.truncate()
 
@@ -20,27 +23,33 @@ write_urls = open('urls.txt', 'w')
 write_urls.truncate()
 
 for repo in results:
-    r = requests.get(repo.html_url)
-    '''
-    this check is just to make sure that the repo still
-    exists. It might not be needed, though omitting this
-    check has caused issues in past projects using the GitHub
-    API, so leaving it in for now
-    '''
-    if r.headers.get('status') is not int(404):
-        print 'processing README for ' + repo.html_url + '...'
-        #writes each repository url to urls.txt
-        write_urls.write(repo.html_url)
-        write_urls.write('\n')
+    if db.repo.find({'repoID': repo.id}):
+        pass
+    else:
+        result = db.repo.insert_one({
+            "repoID" : repo.id
+        })
+        r = requests.get(repo.html_url)
+        '''
+        this check is just to make sure that the repo still
+        exists. It might not be needed, though omitting this
+        check has caused issues in past projects using the GitHub
+        API, so leaving it in for now
+        '''
+        if r.headers.get('status') is not int(404):
+            print 'processing README for ' + repo.html_url + '...'
+            #writes each repository url to urls.txt
+            write_urls.write(repo.html_url)
+            write_urls.write('\n')
 
-        try:
-            readme = repo.get_readme() or ''
-            data = base64.b64decode(readme.content)
-        except:
-            data = ''
+            try:
+                readme = repo.get_readme() or ''
+                data = base64.b64decode(readme.content)
+            except:
+                data = ''
 
-        #writes the readme text, if it exists, to readmeText.txt
-        write_text.write(data)
-        write_text.write('\n\n')
+            #writes the readme text, if it exists, to readmeText.txt
+            write_text.write(data)
+            write_text.write('\n\n')
 
 print 'all done!'
